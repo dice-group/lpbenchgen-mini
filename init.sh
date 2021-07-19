b0b79c6f48da91678948d81a590c1999b8d484de
Lixi <felixconrads@googlemail.com>
2021-07-19 16:01:03 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-wget https://github.com/dice-group/LPBenchGen/releases/download/v2.0.1/lpbenchgen-2.0.1.jar 
+wget https://github.com/dice-group/lpbenchgen-mini/releases/download/v0.1/lpbenchgen-2.0.1-shaded.jar
