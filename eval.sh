15a4a4b5722b23f9da2e7c0ed2198834ac2fb97f
Lixi <felixconrads@googlemail.com>
2021-07-19 11:48:46 +0200

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,5 +1,4 @@
 java -cp lpbenchgen-2.0.1.jar org.dice_group.lpbenchgen.Evaluation --pertain-format gold_standard.ttl test_benchmark.ttl $1 $1_results.tsv
-THRESHOLD=0.3
 MACROF1=`cat $1_results.tsv | grep "Macro F1" | awk -F '\t' '{print $2}'`
 BETTER=`python -c "val = $MACROF1; print('True' if val>=$THRESHOLD else 'False')"`
 echo "$1    MacroF1: $MACROF1    >=THREHSOLD: $BETTER"
